399f8564c6f1f7bd4c03ceb7b2d8909333aa9768
Aneesh Barthakur <abarthakur96@gmail.com>
2016-08-28 02:22:56 +0530

    queue code is compiling , but for some reason not entering initialization loop in init

--- src/shell2.c
+++ src/shell2.c
@@ -20,151 +20,107 @@ typedef struct {
 void  execute(char **argv);
 void  parse(char *line, char **argv);
 
-
-void init_queue(historyQueue *queue, int n){
-    int i;
-    
-    queue->q_size=n;
-    queue->head=0;
-    queue->tail=-1;
-    queue->commandSize=100;
-    queue->history=(char **) malloc(n*sizeof(char *));
-    for (i=0;i++;i<n){
-        *((queue->history)+i)=malloc ((queue->commandsize)*sizeof(char *));
-        (*((queue->history)+i))[0]="\0";
-    }
-};
-
-void resize_queue(historyQueue *queue,int n){
-    char **newHistory=(char **) malloc(n*sizeof(char *)); 
-
-    if (n > queue->q_size){//upsize
-        for (i=0; i++;i <queue->q_size){
-            *(newHistory+i)=*(queue->history+(queue->head +i)%(queue->q_size));
-        }
-        for (i=queue->q_size;i++;i<n){
-            *(newHistory+i)=malloc ((queue->commandsize)*sizeof(char *));
-        }
-        free(queue->history);
-        queue->history=newHistory;
-        if (queue->tail < queue->head){//can never be equal. if less, less only by 1, since dequeue is called only from enqueue . necessity of dequeue? if greater, head=0
-            queue->tail = queue->q_size -1 - queue->tail;
-        }
-        queue->q_size = n;
-        queue->head=0;
-    }
-
-    else if (n < queue->q_size){
-        for (i=0; i++;i <n){
-           *(newHistory+i)=*(queue->history+(queue->head +i));
-        }
-        for (i=n;i++;i<q->size){
-            free(*(queue->history+(queue->head +i)%(queue->q_size)));    
-        }
-        free(queue->history);
-        queue->history=newHistory;
-        if (queue->tail < queue->head || queue->tail >= n ){//can never be equal. if less, less only by 1, since dequeue is called only from enqueue . necessity of dequeue?
-            queue->tail = queue->q_size - 1;
-        }
-        queue->q_size = n;
-        queue->head=0;
-    }
-}
-
-void enqueue(historyQueue *queue, char *newCommand){
-    // int head =queue->head;
-    
-    // int tail = queue->tail;
-    if((queue->tail +1)%(queue->size)==queue->head && queue->tail!=-1){ //boundary condition
-        dequeue(historyQueue,NULL);
-    }
-
-    queue->tail = (queue->tail + 1)%(queue->q_size);
-    //maxsize check here
-    char *dest= *(queue->history + queue->tail);
-    strcpy(dest,newCommand);
-}
-
-dequeue(historyQueue *queue,char *buf){
-    if (buf!=NULL){
-        strcpy(buf,*(queue->history + queue->head));
-    }
-    queue->head=(queue->head+1)%(queue->q_size);
-}
-
-
+void init_queue(historyQueue *queue, int n);
+void resize_queue(historyQueue *queue,int n);
+void enqueue(historyQueue *queue, char *newCommand);
+void dequeue(historyQueue *queue,char *buf);
+void printQueue(historyQueue *queue);
 
 //handle errors, segmentation faults etc.
 int main(void){
 
+    historyQueue q;
+    init_queue(&q,4);
+    // printf("%d  %p\n",q.tail,q.history);
+
+    // printQueue(&q);
+    // enqueue(&q,"1");
+    // enqueue(&q,"2");
+    // enqueue(&q,"3");
+    // enqueue(&q,"4");
+    // enqueue(&q,"5");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     //dynamically allocate memory instead
-    char line[1024];
-    int count=0, size, i;
-    //dynamically allocate, make configurable
-    char *hist[10];
+    // char line[1024];
+    // int count=0, size, i;
+    // //dynamically allocate, make configurable
+    // char *hist[10];
 
-    //array of strings?
-    char  *argv[64];              
+    // //array of strings?
+    // char  *argv[64];              
     
-    //initialize elsewhere
-    //introduce "environment"
-    //instead initialize to some x
-    for(i=0;i<10;i++)
-        hist[i]=NULL;
+    // //initialize elsewhere
+    // //introduce "environment"
+    // //instead initialize to some x
+    // for(i=0;i<10;i++)
+    //     hist[i]=NULL;
 
 
-    while(1){
-        //add user info here+pwd
-        //READ
-        printf(">>$:");
+    // while(1){
+    //     //add user info here+pwd
+    //     //READ
+    //     printf(">>$:");
 
-        //improve readline. first try using gnu c readline lib
-        //then try implementing some of it.
-        gets(line);
+    //     //improve readline. first try using gnu c readline lib
+    //     //then try implementing some of it.
+    //     gets(line);
 
-        //why?
-        size=strlen(line);
+    //     //why?
+    //     size=strlen(line);
 
-        //make cleaner , what the hell is happening here?
-        //why are we allocating exactly that much space? makes no sense!
-        //overhead, completely change this part
-        if(hist[count]!=NULL)
-            hist[count]=(char*)realloc(hist[count], (size+1)*sizeof(char));
-        else        
-            hist[count]=(char *)malloc((size+1)*sizeof(char));
+    //     //make cleaner , what the hell is happening here?
+    //     //why are we allocating exactly that much space? makes no sense!
+    //     //overhead, completely change this part
+    //     if(hist[count]!=NULL)
+    //         hist[count]=(char*)realloc(hist[count], (size+1)*sizeof(char));
+    //     else        
+    //         hist[count]=(char *)malloc((size+1)*sizeof(char));
         
-        //where is the queue implementation of history? 
-        //how are we going to be able to rotate? checking for full buffer?
-        {//ok, why the curly brackets?
-            strcpy(hist[count], line);
+    //     //where is the queue implementation of history? 
+    //     //how are we going to be able to rotate? checking for full buffer?
+    //     {//ok, why the curly brackets?
+    //         strcpy(hist[count], line);
 
-            count++;}
+    //         count++;}
 
         
 
 
-        printf("\n");
-        parse(line, argv); 
-        //check if history? change this to check for builtin commands
-        //!! and !n
+    //     printf("\n");
+    //     parse(line, argv); 
+    //     //check if history? change this to check for builtin commands
+    //     //!! and !n
 
-        //implement cd , ls,bg,fg,kill,jobs,alias,wait,exit,clear,set,reset
-        if(strcmp(argv[0], "history")==0)
-        {
-            for(i=0;i<count;i++)
-            {   
-                if(hist[i]!=NULL)           
-                    printf("%s\n",hist[i]);
-            }
-            printf("\n");
-            continue;
-        }
+    //     //implement cd , ls,bg,fg,kill,jobs,alias,wait,exit,clear,set,reset
+    //     if(strcmp(argv[0], "history")==0)
+    //     {
+    //         for(i=0;i<count;i++)
+    //         {   
+    //             if(hist[i]!=NULL)           
+    //                 printf("%s\n",hist[i]);
+    //         }
+    //         printf("\n");
+    //         continue;
+    //     }
 
-            if (strcmp(argv[0], "exit") == 0)  
-                break; //return from shell to main          
-            execute(argv);          
-    }
-    return 0;
+    //         if (strcmp(argv[0], "exit") == 0)  
+    //             break; //return from shell to main          
+    //         execute(argv);          
+    // }
+    // return 0;
 }
 
 
@@ -191,7 +147,7 @@ void  parse(char *line, char **argv)
     //         line++;            
     //  }
     //  *argv = '\0';                 
-    
+
 
 
 
@@ -227,7 +183,100 @@ void  execute(char **argv)
 }          
 
 
+void init_queue(historyQueue *queue, int n){
+    int i=0;
+    
+    queue->q_size=n;
+    queue->head=0;
+    // queue->tail=-1;
+    queue->tail=3;
+    queue->commandSize=100;
+    queue->history=(char **) malloc(n*sizeof(char *));
+    printf("got here %d\n",n);
+    for (i=0;i++;i<n){
+        printf("aaa\n");
+        printf("%p\n",*(queue->history+i));
+        *((queue->history)+i)=malloc ((queue->commandSize)*sizeof(char));
+        printf("%p\n",*(queue->history+i));
+        // (*((queue->history)+i))[0]='\0';
 
+        (*((queue->history)+i))[0]='a';
+        (*((queue->history)+i))[1]='\0';
+    }
+    printf("even here\n");
+}
+
+void resize_queue(historyQueue *queue,int n){
+    char **newHistory=(char **) malloc(n*sizeof(char *)); 
+    int i;
+
+    if (n > queue->q_size){//upsize
+        for (i=0; i++;i <queue->q_size){
+            *(newHistory+i)=*(queue->history+(queue->head +i)%(queue->q_size));
+        }
+        for (i=queue->q_size;i++;i<n){
+            *(newHistory+i)=malloc ((queue->commandSize)*sizeof(char *));
+        }
+        free(queue->history);
+        queue->history=newHistory;
+        if (queue->tail < queue->head){//can never be equal. if less, less only by 1, since dequeue is called only from enqueue . necessity of dequeue? if greater, head=0
+            queue->tail = queue->q_size -1 - queue->tail;
+        }
+        queue->q_size = n;
+        queue->head=0;
+    }
+
+    else if (n < queue->q_size){
+        for (i=0; i++;i <n){
+           *(newHistory+i)=*(queue->history+(queue->head +i));
+        }
+        for (i=n;i++;i<queue->q_size){
+            free(*(queue->history+(queue->head +i)%(queue->q_size)));    
+        }
+        free(queue->history);
+        queue->history=newHistory;
+        if (queue->tail < queue->head || queue->tail >= n ){//can never be equal. if less, less only by 1, since dequeue is called only from enqueue . necessity of dequeue?
+            queue->tail = queue->q_size - 1;
+        }
+        queue->q_size = n;
+        queue->head=0;
+    }
+}
+
+void enqueue(historyQueue *queue, char *newCommand){
+    // int head =queue->head;
+    
+    // int tail = queue->tail;
+    if((queue->tail +1)%(queue->q_size)==queue->head && queue->tail!=-1){ //boundary condition
+        dequeue(queue,NULL);
+    }
+    printf("a\n");
+    queue->tail = (queue->tail + 1)%(queue->q_size);
+    printf("%d\n",queue->tail);
+    //maxsize check here
+    // char *dest= *(queue->history + queue->tail);
+    char *dest=malloc(20*sizeof(char));
+    strcpy(dest,newCommand);
+}
+
+void dequeue(historyQueue *queue,char *buf){
+    if (buf!=NULL){
+        strcpy(buf,*(queue->history + queue->head));
+    }
+    queue->head=(queue->head+1)%(queue->q_size);
+}
+
+
+void printQueue(historyQueue *queue){
+    int i;
+    if (queue->tail==-1){
+        return ;
+    }
+    while( ((queue->head +i)%(queue->q_size))!= (queue->tail)){
+            printf("%d . %s\n",i,*(queue->history+(queue->head +i)%(queue->q_size)));
+            i++;
+        }
+}
 
 
 
