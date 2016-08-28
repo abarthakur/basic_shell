a87a0dc6d1fc54dba75cafecba7fa1b019f4d887
Aneesh Barthakur <abarthakur96@gmail.com>
2016-08-28 14:53:37 +0530

    ! ! and ! n and history ALMOST working

--- src/shell2.c
+++ src/shell2.c
@@ -6,6 +6,10 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <ctype.h>
+
+#define ARGMAX 50
+
 
 typedef struct {
         int q_size;
@@ -17,8 +21,12 @@ typedef struct {
     } historyQueue;
 
 
+typedef void (*Command)(char **argv,historyQueue *q);
+
+void history_cmd(char **argv,historyQueue *q);
+int custom_execute(char **argv,historyQueue *q);
 void  execute(char **argv);
-void  parse(char *line, char **argv);
+void  parse(historyQueue *q, char *line);
 
 void init_queue(historyQueue *queue, int n);
 void resize_queue(historyQueue *queue,int n);
@@ -31,87 +39,29 @@ int main(void){
 
     historyQueue q;
     init_queue(&q,4);
-    printf("%d  %p\n",q.tail,q.history);
-
-    // printQueue(&q); //no output
-    // enqueue(&q,"1");
-    // printQueue(&q);
-    // enqueue(&q,"2");
-    // printQueue(&q);
-    // enqueue(&q,"3");
-    // printQueue(&q);
-    // enqueue(&q,"4");
-    // printQueue(&q);
-    // enqueue(&q,"5");
-    // printQueue(&q);
-    // enqueue(&q,"6");
-    // printQueue(&q);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     //dynamically allocate memory instead
-    // char line[1024];
-    // int count=0, size, i;
-    // //dynamically allocate, make configurable
-    // char *hist[10];
-
-    // //array of strings?
+    char *line= (char *) malloc(1024 * sizeof(char));
+    //array of strings?
     // char  *argv[64];              
     
-    // //initialize elsewhere
-    // //introduce "environment"
-    // //instead initialize to some x
-    // for(i=0;i<10;i++)
-    //     hist[i]=NULL;
+    //introduce "environment"
 
+    while(1){
+        //add user info here+pwd
+        //READ
+        printf("\n>>$:");
+        //improve readline. first try using gnu c readline lib
+        //then try implementing some of it.
+        gets(line);
 
-    // while(1){
-    //     //add user info here+pwd
-    //     //READ
-    //     printf(">>$:");
-
-    //     //improve readline. first try using gnu c readline lib
-    //     //then try implementing some of it.
-    //     gets(line);
-
-    //     //why?
-    //     size=strlen(line);
-
-    //     //make cleaner , what the hell is happening here?
-    //     //why are we allocating exactly that much space? makes no sense!
-    //     //overhead, completely change this part
-    //     if(hist[count]!=NULL)
-    //         hist[count]=(char*)realloc(hist[count], (size+1)*sizeof(char));
-    //     else        
-    //         hist[count]=(char *)malloc((size+1)*sizeof(char));
+        //add to history
+        enqueue(&q, line);
         
-    //     //where is the queue implementation of history? 
-    //     //how are we going to be able to rotate? checking for full buffer?
-    //     {//ok, why the curly brackets?
-    //         strcpy(hist[count], line);
+        parse(&q,line); 
+        //check if history? change this to check for builtin commands
+        //!! and !n
 
-    //         count++;}
-
-        
-
-
-    //     printf("\n");
-    //     parse(line, argv); 
-    //     //check if history? change this to check for builtin commands
-    //     //!! and !n
-
-    //     //implement cd , ls,bg,fg,kill,jobs,alias,wait,exit,clear,set,reset
+        //implement cd , ls,bg,fg,kill,jobs,alias,wait,exit,clear,set,reset
     //     if(strcmp(argv[0], "history")==0)
     //     {
     //         for(i=0;i<count;i++)
@@ -126,13 +76,13 @@ int main(void){
     //         if (strcmp(argv[0], "exit") == 0)  
     //             break; //return from shell to main          
     //         execute(argv);          
-    // }
-    // return 0;
+    }
+    return 0;
 }
 
 
 
-void  parse(char *line, char **argv)
+void  parse(historyQueue *q, char *line)
 {   
     
     //ok so basically removes the whitespace?
@@ -140,26 +90,95 @@ void  parse(char *line, char **argv)
     // at the very lowest level , history? piping?
     //what does a parser even do?
     //tokenization?
-    // while (*line != '\0') //end of line
-    // {       
-    //     while (*line == ' ' || *line == '\t' || *line == '\n')
-    //         *line++ = '\0';     //why am i setting the next to \0?
-        
+    int i,count=0;
+    char *mybuf = (char *)malloc(1024*sizeof(char));
+    strcpy(mybuf,line);
+    char **argv=malloc(ARGMAX*sizeof(char *));
+    for (i=0;i<ARGMAX;i++){
+        argv[i]=NULL;
+    }
+    int start=0;
+    while (*(mybuf) != '\0') //end of mybuf
+    {       
+        if (*(mybuf) == ' ' || *(mybuf) == '\t' || *(mybuf) == '\n'){
+            // printf("1\n");
+            start=0;
+            *(mybuf) = '\0';     
+        }
+        else if (*(mybuf) == '>' || *(mybuf)== '|' ){
+            // printf("2\n");
+            start==0;
+        }
+        else{
+            // printf("3\n");
+            if (start ==0){
+                // printf("4\n");
+                start =1;
+                argv[count]=mybuf;
+                count++;
+            }
+        }        
+        mybuf++;
+    }
+    i=0;
+    while(argv[i]!=NULL){
+        // printf("ARG %d. %s\n ",i+1,argv[i]);
+        i++;
+    }
 
-    //     printf("got here\n");
-    //     *argv++ = line;          
-    //     // printf("ARG : %s\n",*argv);
-    //     printf("didn't get here\n");
-    //     while (*line != '\0' && *line != ' ' && *line != '\t' && *line != '\n') 
-    //         line++;            
-    //  }
-    //  *argv = '\0';                 
+    char *repeat=(char *)malloc((q->commandSize)*sizeof(char));
+    char *src;
+    int x;
 
+    if(argv[0][0]=='!'){
+        if ( strcmp(argv[0],"!!")==0){
+            // printf("here\n");
+            q->tail= (q->tail -1)%(q->q_size);
+            strcpy(repeat,*(q->history+q->tail));        
+            enqueue(q,repeat);
+            return parse(q,repeat);
+        }
+        else {//assume that it will come correctly
+            x=atoi(argv[0]+1);
+            if(x!=0){
+                src= *(q->history + (q->head + (x-1))%(q->q_size));
+                strcpy(repeat,src);
+                q->tail= (q->tail -1)%(q->q_size);
+                enqueue(q,repeat);
+                return parse(q,repeat);       
+            }
+        }
+        return;
+    }
 
+    if (custom_execute(argv,q)==1){
+        execute(argv);
+    }
+}
 
-
+int custom_execute(char **argv,historyQueue *q){
+    // char **custom_commands={"pwd","cd","jobs","bg","history"};
+    const char *custom_commands[]={"history"};
+    // Command *custom = {NULL. NULL, NULL , NULL, NULL, NULL};
+    Command custom[]= {history_cmd};
+    int cust_no=1;
+    int j=0;
+    // return 1;//comment out later
+    for (j=0;j<cust_no;j++){
+        if (strcmp(argv[0],custom_commands[j])==0){
+            custom[j](argv,q);       
+            return 0;
+        }
+    }
+    
 
 }
+
+
+void history_cmd(char **argv,historyQueue *q){
+    printQueue(q);
+}
+
 
 void  execute(char **argv)
 {
@@ -190,27 +209,53 @@ void  execute(char **argv)
 }          
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 void init_queue(historyQueue *queue, int n){
     int i=0;
     
     queue->q_size=n;
     queue->head=0;
     queue->tail=-1;
-    // queue->tail=3;
     queue->commandSize=100;
     queue->history=(char **) malloc(n*sizeof(char *));
-    // printf("got here %d\n",n);
     for (i=0;i<n;i++){
-        // printf("aaa\n");
-        // printf("%p\n",*(queue->history+i));
         *((queue->history)+i)=(char *)malloc ((queue->commandSize)*sizeof(char));
-        // printf("%p\n",*(queue->history+i));
         (*((queue->history)+i))[0]='\0';
 
-        // (*((queue->history)+i))[0]='a';
-        // (*((queue->history)+i))[1]='\0';
     }
-    // printf("even here\n");
 }
 
 void resize_queue(historyQueue *queue,int n){
